ae8f9902a0ed0c7ccd915fd516c44f46672799ed
mikemalkhasyan <mkrtich.malkhasyan1996@gmail.com>
2021-07-10 16:28:47 +0400

    - Map: does the map have key?

--- Map-Exercises/01_04/map_has_begin.js
+++ Map-Exercises/01_04/map_has_begin.js
@@ -10,4 +10,8 @@ saturday.set(12, 'lunch');
 saturday.set(3, 'watch a movie');
 
 //Does our Map have a value associated with 4?
+const hasFour = saturday.has(4);
+console.log("Does my Map have anything associated with 4?", hasFour);
 //Does our Map have a value associated with 8?
+const hasEight = saturday.has(8);
+console.log("Does my Map have anything associated with 8?", hasEight);
